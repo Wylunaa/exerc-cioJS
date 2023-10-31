8b08d76fca11b59b925aa576d0661cd2f332fd13
Wylunaa <wylunaa@gmail.com>
2023-10-30 21:55:20 -0300

    commit geral

---
+++ ex.js
@@ -0,0 +1,11 @@
+const opcaoUsuario = prompt('Você está na "entrada", deseja ir para esquerda ou direita? digite E pra esquerda ou D para direita:');
+
+if(opcaoUsuario === 'E' || opcaoUsuario === 'e'){
+alert('Abrindo a porta 1 !');
+} else if(opcaoUsuario === 'D' || opcaoUsuario === 'd'){
+alert('Abrindo a porta 2 !');
+}else{
+alert('Opcão inválida!')
+}
+
+
