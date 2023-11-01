36092dce6af54f683a367c817bb054570bc5b41f
Wylunaa <wylunaa@gmail.com>
2023-11-01 10:18:33 -0400

    organização

--- ex.js
+++ ex.js
@@ -1,11 +1,10 @@
-const opcaoUsuario = prompt('Você está na "entrada", deseja ir para esquerda ou direita? digite E pra esquerda ou D para direita:');
+//const opcaoUsuario = prompt('Você está na "entrada", deseja ir para esquerda ou direita? digite E pra esquerda ou D para direita:');
 
-if(opcaoUsuario === 'E' || opcaoUsuario === 'e'){
-alert('Abrindo a porta 1 !');
-} else if(opcaoUsuario === 'D' || opcaoUsuario === 'd'){
-alert('Abrindo a porta 2 !');
-}else{
-alert('Opcão inválida!')
-}
-
+//if(opcaoUsuario === 'E' || opcaoUsuario === 'e'){
+//alert('Abrindo a porta 1 !');
+//} else if(opcaoUsuario === 'D' || opcaoUsuario === 'd'){
+//alert('Abrindo a porta 2 !');
+//}else{
+//alert('Opcão inválida!')
+//} 
 
